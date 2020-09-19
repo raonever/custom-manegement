ea73ee05c8a42e55e89f9373f528f4b61f5558ec
raonever <ever29lkh@gmail.com>
2020-09-19 11:08:41 +0900

    create customer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '장길산',
+    'birthday': '971222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '신사임당',
+    'birthday': '981222',
+    'gender': '여자',
+    'job': '대학생'
+  }
+
+]
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h2>Let'st develop management system</h2>
+        {
+          customers.map(customer => {
+            return (
+              <Customer
+                key={customer.id}
+                id={customer.id}
+                image={customer.image}
+                name={customer.name}
+                birthday={customer.birthday}
+                gender={customer.gender}
+                job={customer.job}
+              />
+            )
+          })
+        }
       </div>
     );
   }
