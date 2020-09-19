0e094f176d4d65e607034caae2acd25ddc8bbf7a
raonever <ever29lkh@gmail.com>
2020-09-19 09:51:52 +0900

    custom manegement init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h2>Let'st develop management system</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
